d9de5af1480da2d43d952eb2040b596af6ec8bc1
JimmyKwon <kwonys93@gmail.com>
2020-02-28 18:15:33 +0800

    qqqqq

--- gulpfile.js
+++ gulpfile.js
@@ -15,82 +15,51 @@ var cmd = require('node-cmd'),
     })
 
 
-    function awaitJobCompletion(jobId, maxRC=0, callback, tries = 30, wait = 1000) {
-      if (tries > 0) {
-          sleep(wait);
-          cmd.get(
-          'zowe jobs view job-status-by-jobid ' + jobId + ' --rff retcode --rft string',
-          function (err, data, stderr) {
-              retcode = data.trim();
-              //retcode should either be null of in the form CC nnnn where nnnn is the return code
-              if (retcode == "null") {
-                awaitJobCompletion(jobId, maxRC, callback, tries - 1, wait);
-              } else if (retcode.split(" ")[1] <= maxRC) {
-                callback(null);
-              } else {
-                callback(new Error(jobId + " had a return code of " + retcode + " exceeding maximum allowable return code of " + maxRC));
-              }
-          }
-          );
-      } else {
-          callback(new Error(jobId + " timed out."));
-      }
-    }
-
-
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
     gulp.task('build-cobol', function (callback) {
   //    var command = "zowe endevor generate element " + config.testElement + " --type COBOL --override-signout --maxrc 0 --stage-number 1";
   var command = "zowe  endevor update element FAPCOB05 --env SMPLTEST --sys FINANCE --sub ACCTPAY --typ COBOL --ff FAPCOB05.cbl -i ENDEVOR --comment test225 --ccid abcd5";
   //var  command = "zowe endevor generate element FAPCOB05 --env SMPLTEST --sn 1 --sys FINANCE --sub ACCTPAY --type COBOL --cb -i ENDEVOR --comment test223 --ccid abcd";
   //     var command = "zowe  endevor list elements -i ENDEVOR --env SMPLTEST --sn 1 --sys FINANCE --sub ACCTPAY --typ COBOL" ;
 
-      simpleCommand(command, callback);
+      simpleCommand(command, "command-archive/build-cobol", callback);
     });
 
 
     gulp.task('bind-n-grant', function (callback) {
       var ds = config.bindGrantJCL;
-      submitJob(ds, 4, callback);
+      submitJobAndDownloadOutput(ds, "job-archive/bind-n-grant", 4, callback);
     });
 
     gulp.task('test-data', function (callback) {
       var ds = config.sqlJCL;
-      submitJob(ds, 4, callback);
+      submitJobAndDownloadOutput(ds, "job-archive/test-data", 4, callback);
     });
 
     gulp.task('build-lnk', function (callback) {
       var command = "zowe endevor generate element " + config.testElement + " --type LNK --override-signout --maxrc 0 --stage-number 1";
     
-      simpleCommand(command, callback);
+      simpleCommand(command, "command-archive/build-lnk", callback);
     });
     
     gulp.task('build', gulpSequence('build-cobol','build-lnk'));
     
 
     gulp.task('cics-refresh', function (callback) {
-      var command = 'zowe cics refresh program "' + config.cicsProgram + '"' + ' --region-name "' + config.cicsRegion +'"';
+      var command = 'zowe cics refresh program "' + config.cicsProgram + '"';
     
-      simpleCommand(command, callback);
+      simpleCommand(command, "command-archive/cics-refresh", callback);
     });
     
     gulp.task('copy-dbrm', function (callback) {
- //  var command = 'zowe file-master-plus copy data-set "' + config.devDBRMLIB + '" "' + config.testDBRMLIB + '" -m ' + config.testElement;
-   var command = 'zowe zos-extended-files copy data-set "KWOYO01.JCL(A)" "KWOYO01.EDVR.JCL(A)" --replace';
- //  var command = 'zowe zos-extended-files copy data-set "' + config.devDBRMLIB + '" "' + config.testDBRMLIB + '" -m ' + config.testElement;
-      
-      simpleCommand(command, callback);
+      var command = 'zowe file-master-plus copy data-set "' + config.devDBRMLIB + '" "' + config.testDBRMLIB + '" -m ' + config.testElement;
+    
+      simpleCommand(command, "command-archive/copy-dbrm", callback);
     });
     
     gulp.task('copy-load',  function (callback) {
-      var command = 'zowe zos-extended-files copy data-set "KWOYO01.JCL(B)" "KWOYO01.EDVR.JCL(B)" --replace';
-      //var command = 'zowe file-master-plus copy data-set "' + config.devLOADLIB + '" "' + config.testLOADLIB + '" -m ' + config.testElement;
+      var command = 'zowe file-master-plus copy data-set "' + config.devLOADLIB + '" "' + config.testLOADLIB + '" -m ' + config.testElement;
     
-      simpleCommand(command, callback);
+      simpleCommand(command, "command-archive/copy-load", callback);
     });
     
     gulp.task('deploy', gulpSequence('copy-dbrm','copy-load','bind-n-grant','cics-refresh'));
@@ -224,12 +193,18 @@ function createAndSetProfiles(host, user, pass, callback){
 * @param {awaitJobCallback} callback          function to call after completion
 * @param {Array}            [expectedOutputs] array of expected strings to be in the output
 */
-function simpleCommand(command, callback){
+function simpleCommand(command, dir, callback, expectedOutputs){
   cmd.get(command, function(err, data, stderr) { 
+    //log output
+    var content = "Error:\n" + err + "\n" + "StdErr:\n" + stderr + "\n" + "Data:\n" + data;
+    writeToFile(dir, content);
+    
     if(err){
       callback(err);
     } else if (stderr){
       callback(new Error("\nCommand:\n" + command + "\n" + stderr + "Stack Trace:"));
+    } else if(typeof expectedOutputs !== 'undefined'){
+      verifyOutput(data, expectedOutputs, callback);
     } else {
       callback();
     }
@@ -243,33 +218,32 @@ function simpleCommand(command, callback){
 * @param {number}           [maxRC=0]           maximum allowable return code
 * @param {awaitJobCallback} callback            function to call after completion
 */
+function submitJobAndDownloadOutput(ds, dir="job-archive", maxRC=0, callback){
+  var command = 'zowe jobs submit data-set "' + ds + '" -d ' + dir + " --rfj";
+//var command = 'zowe jobs submit data-set "KWOYO01.JCL(SAMPJCL1)" --rff jobid --rft string';
 
-function submitJob(ds, maxRC=4, callback){
-  //var command = 'zowe jobs submit data-set "' + ds + '" --vasc'
-   var command = 'zowe jobs submit data-set "' + ds + '" --rff jobid --rft string'
-  // var command = 'zowe jobs submit data-set "KWOYO01.JCL(SAMPJCL1)" --view-all-spool-content';
-  // var command = 'zowe jobs submit data-set "KWOYO01.JCL(SAMPJCL1)" --view-all-spool-content';
- 
-  cmd.get(command, function(err, data, stderr) { 
-     if(err){
-       callback(err);
-     } else if (stderr){
-       callback(new Error("\nCommand:\n" + command + "\n" + stderr + "Stack Trace:"));
-     } else {
-       // Strip unwanted whitespace/newline
-       var jobId = data.trim();
-       
-       // Await the jobs completion
-       awaitJobCompletion(jobId, maxRC, function(err){
-         if(err){
-           callback(err);
-         } else{
-           callback();
-         }
-       });
-     }
-    });
- }
+cmd.get(command, function(err, data, stderr) { 
+    //log output
+    var content = "Error:\n" + err + "\n" + "StdErr:\n" + stderr + "\n" + "Data:\n" + data;
+    writeToFile("command-archive/job-submission", content);
+
+    if(err){
+      callback(err);
+    } else if (stderr){
+      callback(new Error("\nCommand:\n" + command + "\n" + stderr + "Stack Trace:"));
+    } else {
+      data = JSON.parse(data).data;
+      retcode = data.retcode;
+
+      //retcode should be in the form CC nnnn where nnnn is the return code
+      if (retcode.split(" ")[1] <= maxRC) {
+        callback(null);
+      } else {
+        callback(new Error("Job did not complete successfully. Additional diagnostics:" + JSON.stringify(data,null,1)));
+      }
+    }
+  });
+}
 
 /**
 * Submits multiple simple commands
